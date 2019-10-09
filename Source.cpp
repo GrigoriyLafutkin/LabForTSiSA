58f9357dd986566fc31024783c0ff6ccab5d90df
Grisha_Lafutkin <43631684+GrigoriyLafutkin@users.noreply.github.com>
2019-10-09 22:38:23 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+// Copyright 2019 GrigoriyLafutkin <grigoriylafutkin@yandex.ru>
+
+#include "Header.hpp"
+
+int main()
+{
+    RandSearchSin5x(9, 12);
+    //RandSearch(9, 12);
+
+    system("pause");
+}

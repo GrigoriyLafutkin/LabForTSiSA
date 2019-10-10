f096e38d0b67a487d1b1d020187e9df0400b3afa
Grisha_Lafutkin <43631684+GrigoriyLafutkin@users.noreply.github.com>
2019-10-10 12:30:53 +0300

    Create Source2.cpp

---
+++ Source2.cpp
@@ -0,0 +1,13 @@
+// Copyright 2019 GrigoriyLafutkin <grigoriylafutkin@yandex.ru>
+
+#include "Header.hpp"
+
+int main()
+{
+    float a = 9;
+    float b = 12;
+
+    FibSearch(a, b);
+
+    system("pause");
+}
